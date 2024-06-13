8ed4eee84ffac7f8b3abbbdb4bb3dac006fa41ef
Omnitok <94868134+Omnitok@users.noreply.github.com>
2024-06-13 10:10:32 +0200

    Add files via upload

--- tools/run_inference.sh
+++ tools/run_inference.sh
@@ -5,7 +5,7 @@ docker  run \
     -u $(id -u):$(id -g) \
     --gpus all \
     -v $PWD:$PWD \
-    cuda-tensorflow /bin/bash -c "python3 $REPO_ROOT/tools/run_inference.py \
-        --model $REPO_ROOT/out/training_06_augmented/model.onnx \
-        --input_folder $REPO_ROOT/data/cropped_/batch_1/cropped_original_png \
-        --output_folder $REPO_ROOT/data/cropped_/batch_1/out"
+    cuda-tensorflow /bin/bash -c "python3 $REPO_ROOT/run_inference.py \
+        --model $REPO_ROOT/m232/model.onnx \
+        --input_folder $REPO_ROOT/input \
+        --output_folder $REPO_ROOT/scs_out/"
